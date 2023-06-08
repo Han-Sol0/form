24dd114c19c56d0cd0dec52365de3ae73a72e789
Valentin <hanolo@yandex.ru>
2023-06-08 22:04:48 +0300

    form v.2

--- form/src/App.js
+++ form/src/App.js
@@ -21,43 +21,81 @@ const setData = (formData) => {
 
 function App() {
     const { getState, updateState } = useStore();
+    const [loginError, setLoginError] = useState(null);
 
     const onSubmit = (event) => {
         event.preventDefault();
         setData(getState());
     };
     const { email, password, repeatPassword } = getState();
-    const onChange = ({ target }) => {
-        updateState(target.name, target.value);
-        if (true) {
-        }
-    };
 
     return (
         <div className="App">
             <form onSubmit={onSubmit}>
+                {loginError && <div className="App-link">{loginError}</div>}
                 <input
                     type="email"
                     name="email"
                     value={email}
                     placeholder="Почта"
-                    onChange={onChange}
+                    onChange={({ target }) => {
+                        updateState(target.name, target.value);
+
+                        let error = null;
+
+                        if (
+                            !/^\S{2,}@\S{2,}\.[a-zA-Z]{2,10}$/.test(
+                                target.value
+                            )
+                        ) {
+                            error = "Неверный логин.";
+                        }
+                        setLoginError(error);
+                    }}
                 />
                 <input
                     type="password"
                     name="password"
                     value={password}
                     placeholder="Пароль"
-                    onChange={onChange}
+                    onChange={({ target }) => {
+                        updateState(target.name, target.value);
+
+                        let error = null;
+
+                        if (
+                            !(
+                                /^\S+$/.test(target.value) &&
+                                /[a-zA-Z]+/.test(target.value) &&
+                                /[0-9]+/.test(target.value) &&
+                                /[\W_]+/.test(target.value)
+                            )
+                        ) {
+                            error =
+                                "Пароль не удовлетворяет политикам безопасности.";
+                        }
+                        setLoginError(error);
+                    }}
                 />
                 <input
                     type="password"
                     name="repeatPassword"
                     value={repeatPassword}
                     placeholder="Повтор пароля"
-                    onChange={onChange}
+                    onChange={({ target }) => {
+                        updateState(target.name, target.value);
+
+                        let error = null;
+
+                        if (target.value !== password) {
+                            error = "Пароли не совпадают.";
+                        }
+                        setLoginError(error);
+                    }}
                 />
-                <button type="submit">Зарегистрироваться</button>
+                <button type="submit" disabled={loginError !== null}>
+                    Зарегистрироваться
+                </button>
             </form>
         </div>
     );
